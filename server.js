e98c8a6cfbc95b07e4379644fe10f60786d10454
JoelSajiputhenpurakkal <86317289+JoelSajiputhenpurakkal@users.noreply.github.com>
2024-08-19 16:15:52 +0530

    Update server.js

--- server.js
+++ server.js
@@ -1,28 +1,27 @@
 const express = require('express');
 const mongoose = require('mongoose');
 const dotenv = require('dotenv');
+const cors = require('cors'); // Import the cors package
 const recipeRoutes = require('./routes/recipeRoutes');
 
 dotenv.config();
 const app = express();
 const PORT = process.env.PORT || 5000;
 
+// Database Connection
 const conn = require('./services/db');
 conn.dbConnection();
 
+// Enable CORS for all origins (You can specify specific origins if needed)
+app.use(cors());
 
+// Middleware to parse JSON requests
 app.use(express.json());
+
+// Routes
 app.use('/api/recipes', recipeRoutes);
 
-// mongoose.connect(process.env.MONGO_URI, {
-//   useNewUrlParser: true,
-//   useUnifiedTopology: true,
-// }).then(() => {
-//   console.log('Connected to MongoDB');
-// }).catch(err => {
-//   console.error('Error connecting to MongoDB:', err);
-// });
-
+// Start the server
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
 });
