aa09bd3117a166e2078cf1176a5c7e93e68e4f17
joelsaji <joelsajiputhenpurakkal@gmail.com>
2024-08-19 13:15:07 +0530

    commit vercel

--- controllers/recipeController.js
+++ controllers/recipeController.js
@@ -2,12 +2,14 @@ const Recipe = require('../models/Recipe');
 
 exports.addToWishlist = async (req, res) => {
   try {
+    console.log(req.body);
     const { label, uri, image, source, url } = req.body;
     const newRecipe = new Recipe({ label, uri, image, source, url });
     await newRecipe.save();
     res.status(201).json({ message: 'Recipe added to wishlist' });
   } catch (error) {
     res.status(500).json({ error: 'Failed to add recipe to wishlist' });
+    console.log(error);
   }
 };
 
--- server.js
+++ server.js
@@ -7,17 +7,21 @@ dotenv.config();
 const app = express();
 const PORT = process.env.PORT || 5000;
 
+const conn = require('./services/db');
+conn.dbConnection();
+
+
 app.use(express.json());
 app.use('/api/recipes', recipeRoutes);
 
-mongoose.connect(process.env.MONGO_URI, {
-  useNewUrlParser: true,
-  useUnifiedTopology: true,
-}).then(() => {
-  console.log('Connected to MongoDB');
-}).catch(err => {
-  console.error('Error connecting to MongoDB:', err);
-});
+// mongoose.connect(process.env.MONGO_URI, {
+//   useNewUrlParser: true,
+//   useUnifiedTopology: true,
+// }).then(() => {
+//   console.log('Connected to MongoDB');
+// }).catch(err => {
+//   console.error('Error connecting to MongoDB:', err);
+// });
 
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
